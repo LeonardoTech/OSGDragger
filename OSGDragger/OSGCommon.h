835bca91838f109429f87af55a0e49cc4b002e44
LeonardoSun <54fengyi@sohu.com>
2015-05-25 10:57:12 +0800

    New: init.

---
+++ OSGDragger/OSGCommon.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <osgViewer/Viewer>
+#include <osgDB/ReadFile>
+#include <osg/Node>
+#include <osg/Geode>
+#include <osg/Geometry>
+#include <osg/ShapeDrawable>
+#include <osgGA/GUIEventHandler>
+#include <osgUtil/LineSegmentIntersector>
+#include <osgFX/Scribe>
+#include <osg/Camera>
+#include <osg/GraphicsContext>
+#include <osg/MatrixTransform>
